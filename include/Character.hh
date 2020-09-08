d8f9dedbda477dea0941a3964bdfe708664c44ba
Jesus Armando Zamora Gamez <armando.zamora14@hotmail.com>
2020-09-08 10:41:50 -0700

    Struct

--- include/Character.hh
+++ include/Character.hh
@@ -1,20 +1,19 @@
-#include <iostream>
+#include<iostream>
 
 class Character
 {
     private:
-        uint32_t health;
+        short int health;
         uint32_t strength;
         uint32_t exp;
         short int luck;
-        std::string lore; //not initial
+        std::string lore;//not initial
         uint32_t speed;
-
     public:
-        Character(short int, uint32_t, uint32_t, short int, uint32_t); //constructor
-        ~Character(); //destructor
-        short int GetHealth() const; //todo lo que pongas dentro de esto no lo puedes modificar, todo es inmutable
-        uint32_t GetStrength() const;
+        Character(short int, uint32_t, uint32_t, short int, uint32_t);//constructor
+        ~Character();//destructor
+        short int GetHealth() const;//todo lo que pongas dentro de esto, no lo puedes modificar, osea todo es inmutable
+        uint32_t GetStrenght() const;
         uint32_t GetExp() const;
         short int GetLuck() const;
         std::string GetLore() const;
@@ -22,5 +21,4 @@ class Character
 
         void SetLore(std::string);
         void GetDamage(short int damage);
-
 };
